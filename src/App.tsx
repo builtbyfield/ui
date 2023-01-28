ce620ebb1d8436442938c4ffefad5af552ea292c
Jesse McLean <essejmclean@gmail.com>
2023-01-28 14:13:52 +0000

    feat: select

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,26 @@
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
 
 function App() {
-
   return (
-    <div>
-      
+    <div className="flex p-8 flex-col items-center">
+      <div className="w-full max-w-md p-4 rounded-xl border">
+        <Select>
+          <SelectTrigger className="w-[180px]">
+            <SelectValue placeholder="Theme" />
+          </SelectTrigger>
+          <SelectContent>
+            <SelectItem value="light">Light</SelectItem>
+            <SelectItem value="dark">Dark</SelectItem>
+            <SelectItem value="system">System</SelectItem>
+          </SelectContent>
+        </Select>
+      </div>
     </div>
   );
 }
